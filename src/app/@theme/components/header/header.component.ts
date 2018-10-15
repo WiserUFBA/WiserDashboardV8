fcd0081d5eb55768538368477b2291880112bc90
jefersonla <oi_je@hotmail.com>
2018-10-15 19:21:25 -0300

    Dashboard de exemplo para IoT

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -28,10 +28,11 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.userService.getUsers()
       .subscribe((users: any) => this.user = users.nick);
+    this.sidebarService.toggle(false, 'settings-sidebar');
   }
 
   toggleSidebar(): boolean {
-    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.sidebarService.toggle(false, 'menu-sidebar');
     this.layoutService.changeLayoutSize();
 
     return false;
